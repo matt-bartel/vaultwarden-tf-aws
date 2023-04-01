9f988b39e16dc2d8700171ed990e249ca6e6b3d1
Matt Bartel <mattbartel3@gmail.com>
2023-03-31 19:53:35 -0500

    fix: swap docker-compose for 'docker compose'

--- templates/backup.sh
+++ templates/backup.sh
@@ -3,11 +3,11 @@
 set -e
 
 cd /home/ubuntu/compose
-docker-compose down
+docker compose down
 cd ..
 tar --xz -cf bitwarden-backup.tar.xz bitwarden letsencrypt
 /usr/local/bin/aws s3 cp bitwarden-backup.tar.xz s3://${bucket}/bitwarden-backup.tar.xz --sse
 rm bitwarden-backup.tar.xz
 cd compose
-docker-compose up -d
+docker compose up -d
 
